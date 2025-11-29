c33fb87582193c1654200ec840cc1c28b69ca018
OneNc <muhamadtriwanda@gmail.com>
2025-11-29 10:23:14 +0700

    Add path Teras.Background

--- config/services.php
+++ config/services.php
@@ -34,5 +34,9 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'teras_background' => [
+        'exe_path' => env('TERAS_BACKGROUND_EXE'),
+        'exe_path_edmicmd' => env('TERAS_BACKGROUND_EDMICMD_EXE'),
+    ],
 
 ];
