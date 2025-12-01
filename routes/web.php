ee68802b538980f6412b3c325968f81dcf76a09f
OneNc <muhamadtriwanda@gmail.com>
2025-12-02 04:22:56 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,7 @@ Route::middleware('auth')->group(function () {
     Route::post('/meter', [MeterController::class, 'store'])->name('meter.store');
     Route::get('/meter/index/data', [MeterController::class, 'api'])->name('api.meter');
     Route::get('/meter/overview/{id}', [MeterController::class, 'overview'])->name('meter.overview');
-    Route::post('/meter/overview/{id}/read', [MeterController::class, 'read'])->name('meter.overview.read');
-
+    Route::post('/meter/overview/read', [MeterController::class, 'read'])->name('meter.overview.read');
     // Group
     Route::get('/group', [GroupController::class, 'index'])->name('group');
     Route::post('/group', [GroupController::class, 'store'])->name('group.store');
@@ -38,13 +37,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/report/loadprofile', [LoadProfileController::class, "index"])->name('report.loadprofile');
     Route::get('/report/loadprofile/data', [LoadProfileController::class, 'api'])->name('report.loadprofile.api');
     Route::post('/report/loadprofile/export/', [LoadProfileController::class, 'export'])->name('report.loadprofile.export');
-    Route::post('/report/loadprofile/{id}/read', [LoadProfileController::class, 'read'])->name('report.loadprofile.read');
-    Route::get('/report/loadprofile/{id}/update', [LoadProfileController::class, 'api_update'])->name('report.loadprofile.api.update');
+    Route::post('/report/loadprofile/read', [LoadProfileController::class, 'read'])->name('report.loadprofile.read');
 
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
-// Route::get('/login', fn() => view('auth.login'))->name('login');
-
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/login', [AuthController::class, 'store'])->name('login.store');
