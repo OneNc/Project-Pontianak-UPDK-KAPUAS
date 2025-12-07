27b8df07ddd924b646b044e0b96d97ffd3e3996e
OneNc <muhamadtriwanda@gmail.com>
2025-12-07 07:50:48 +0700

    add request user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -39,9 +39,79 @@ class DatabaseSeeder extends Seeder
             ],
             [
                 'name' => 'Administrator',
-                'username' => 'administrator',
-                'password' => '$2y$12$s.beQvS6jHmKuZfm7Z0Jp.maIj1HfpUvk/f7zIvCXnZGVmmoDlD8u'
-            ]
+                'username' => 'admin',
+                'password' => bcrypt("PLTDAdministrator")
+            ],
+            [
+                'name' => "ROBBI TAMPUBOLON",
+                'username' => "8409493Z",
+                'password' => bcrypt("P@ssw0rdupks")
+            ],
+            [
+                'name' => "MUHAMMAD SHOLEH",
+                'username' => "93161936ZY",
+                'password' => bcrypt("P@ssw0rdupks")
+            ],
+            [
+                'name' => "FARHAN WIBOWO",
+                'username' => "9112027CY",
+                'password' => bcrypt("P@ssw0rdupks")
+            ],
+            [
+                'name' => "ERWIN SUJATMIKO",
+                'username' => "9216919ZY",
+                'password' => bcrypt("P@ssw0rdsera")
+            ],
+            [
+                'name' => "ELIEZER KRISTIAN AMBARITA",
+                'username' => "9313077CY",
+                'password' => bcrypt("P@ssw0rdsera")
+            ],
+            [
+                'name' => "JUWANDI",
+                'username' => "8709030C",
+                'password' => bcrypt("P@ssw0rdsewie")
+            ],
+            [
+                'name' => "MOHAMMAD REZA FAHLEVI",
+                'username' => "0024126ZJY",
+                'password' => bcrypt("P@ssw0rdsewie")
+            ],
+            [
+                'name' => "FITRA RIA HERMAWAN",
+                'username' => "8908017C",
+                'password' => bcrypt("P@ssw0rdstn")
+            ],
+            [
+                'name' => "ADI PUTRA",
+                'username' => "9514116CY",
+                'password' => bcrypt("P@ssw0rdstn")
+            ],
+            [
+                'name' => "MUHAMMAD AKBAR FADHILA",
+                'username' => "94171212ZY",
+                'password' => bcrypt("P@ssw0rdstg")
+            ],
+            [
+                'name' => "CHOLILUR RAHMAN",
+                'username' => "9514120CY",
+                'password' => bcrypt("P@ssw0rdstg")
+            ],
+            [
+                'name' => "DWI MUHADIYANTORO",
+                'username' => "94191367ZY",
+                'password' => bcrypt("P@ssw0rdktp")
+            ],
+            [
+                'name' => "FERIANSYAH GUNAWAN",
+                'username' => "9616013LAY",
+                'password' => bcrypt("P@ssw0rdktp")
+            ],
+            [
+                'name' => "ITDKP",
+                'username' => "itdkp",
+                'password' => '$2y$12$RDOagD00drd50JDYMdrjGehypRjOnG38y.y0xmrekK6pieLiEYBN2'
+            ],
         ];
 
         foreach ($users as $user) {
